9e70ab7ab83895ea26001e352085bf570c07cceb
menghaonan <menghaonan@jd.com>
2022-04-27 20:37:13 +0800

    最长无重复子串解法二

--- src/main/java/com/example/leecode/最长无重复子串.java
+++ src/main/java/com/example/leecode/最长无重复子串.java
@@ -9,16 +9,19 @@ public class 最长无重复子串 {
 
     public static void main(String[] args) {
         char[] a = {'p', 'w', 'w', 'k', 'e', 'w'};
-        System.out.println("案例一: " + lengthOfLongestSubstring(a));
+        System.out.println("案例一: " + lengthOfLongestSubstring1(a));
+        System.out.println("案例二: " + lengthOfLongestSubstring2(a));
 
         char[] b = {'b', 'b', 'b', 'b'};
-        System.out.println("案例二: " + lengthOfLongestSubstring(b));
+        System.out.println("案例一: " + lengthOfLongestSubstring1(b));
+        System.out.println("案例二: " + lengthOfLongestSubstring2(b));
 
         char[] c = {'a', 'b', 'c', 'a', 'b', 'c', 'b', 'b'};
-        System.out.println("案例三: " + lengthOfLongestSubstring(c));
+        System.out.println("案例一: " + lengthOfLongestSubstring1(c));
+        System.out.println("案例三: " + lengthOfLongestSubstring2(c));
     }
 
-    public static int lengthOfLongestSubstring(char[] s) {
+    public static int lengthOfLongestSubstring1(char[] s) {
         HashMap<Character, Integer> window = new HashMap<Character, Integer>();
         int left = 0, right = 0;
         int res = 0; // 记录结果
@@ -38,5 +41,25 @@ public class 最长无重复子串 {
             res = Math.max(res, right - left);
         }
         return res;
+    }
+
+    public static int lengthOfLongestSubstring2(char[] chas) {
+        if(chas == null || chas.length == 0){
+            return 0;
+        }
+        int pre = -1;
+        int cur = 0;
+        int len = 0;
+        int[] map = new int[256];
+        for(int i = 0; i < 256; i++){
+            map[i] = -1;
+        }
+        for(int i = 0; i < chas.length; i ++){
+            pre = Math.max(pre, map[chas[i]]);
+            cur = i - pre;
+            len = Math.max(len, cur);
+            map[chas[i]] = i;
+        }
+        return len;
     }
 }
